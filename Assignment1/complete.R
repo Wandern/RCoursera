e7adddc54ea760f9b6c883af3b77c0418b4ea1b9
Wandern <daniel@terrakarma.us>
2014-10-16 14:14:47 -0400

    Corrected some syntax, and removed extra un needed code.

--- Assignment1/complete.R
+++ Assignment1/complete.R
@@ -1,5 +1,4 @@
 complete <- function(directory, id = 1:332) {
-  
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
   ## 'id' is an integer vector indicating the monitor ID numbers
@@ -11,21 +10,16 @@ complete <- function(directory, id = 1:332) {
   ## ...
   ## where 'id' is the monitor ID number and 'nobs' is the
   ## number of complete cases
-  
-  
-  # set working directory
-  if(grep("specdata", directory) == 1) {
-    directory <- ("./specdata/")
-  }
+
   # get the length of id vector
   id_len <- length(id)
   complete_data <- rep(0, id_len)
   # find all files in the specdata folder
-  all_files <- as.character( list.files(directory) )
-  file_paths <- paste(directory, all_files, sep="")
+  files_full <- list.files(directory,full.names=TRUE)
+  #file_paths <- paste(directory, all_files, sep="")
   j <- 1
   for (i in id) {
-    current_file <- read.csv(file_paths[i], header=T)
+    current_file <- read.csv(files_full[i], header=T)
     complete_data[j] <- sum(complete.cases(current_file))
     j <- j + 1
   }
